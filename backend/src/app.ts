c6873308254b5478c5d3f75e1ba70e48f40609a4
aaryanrajputhere <itsaaryanrajput@gmail.com>
2025-11-19 01:33:04 +0530

    Added Drive Routes in App.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import materialRoutes from "./routes/materials.routes";
 import vendorRoutes from "./routes/vendors.routes";
 import syncRoutes from "./routes/sync-sheet.routes";
 import awardRoutes from "./routes/awards.routes";
+import driveRoutes from "./routes/drive.routes";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,5 +19,6 @@ app.use("/api/materials", materialRoutes);
 app.use("/api/vendors", vendorRoutes);
 app.use("/api/sync-sheet", syncRoutes);
 app.use("/api/awards", awardRoutes);
+app.use("/api/drive", driveRoutes);
 
 export default app;
