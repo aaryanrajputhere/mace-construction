385ef15ada643a978565530de26e039c2e1d60c4
aaryanrajputhere <itsaaryanrajput@gmail.com>
2025-11-21 01:45:53 +0530

    Removed vendor folder

--- vendors/src/hooks/materials/useDeleteMaterial.tsx
+++
@@ -1,9 +0,0 @@
-import { api } from "../../lib/axios";
-
-export function useDeleteMaterial() {
-  const deleteMaterial = async (id: string) => {
-    await api.delete(`/materials/${id}`);
-  };
-
-  return { deleteMaterial };
-}
